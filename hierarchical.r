9b7e19b1bae3f8e61d1900ca0b1f10031936cd85
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-11-04 01:06:22 +0530

    Working commit

--- hierarchical.r
+++ hierarchical.r
@@ -7,14 +7,18 @@ squared_euclidean_distance <- function(p1,p2) {
   return(sum((p1 - p2)**2))
 }
 
-cluster <- function(data) {
-  min_val = manhattan_distance(data[0,],data[1,])
-  min_i = 0
-  min_j = 1
-  for(i in 1:nrow(data)) {
-    for(j in 1:nrow(data)) {
+get_centroid <- function(l) {
+  return(Reduce("+",l)/length(l))
+}
+
+cluster <- function(data,clusters) {
+  min_val = manhattan_distance(data[[1]],data[[2]])
+  min_i = 1
+  min_j = 2
+  for(i in 1:length(data)) {
+    for(j in 1:length(data)) {
       if( i != j) {
-          distance = manhattan_distance(data[i,],data[j,])
+          distance = manhattan_distance(data[[i]],data[[j]])
           if(distance < min_val) {
             min_val = distance
             min_i = i
@@ -23,6 +27,33 @@ cluster <- function(data) {
       }
     }
   }
+  
+  clusters[[min_i]] <- min_i 
+  clusters[[min_j]] <- min_i 
+  return(clusters)
 }
 
 data <- as.matrix(read.csv("Datasets/movement_libras_1.data",header = FALSE, sep = ",",fill = FALSE))
+clusters <- list()
+for(i in 1:500) {
+  #mat[[i]] <- data[i,]
+  clusters[[i]] <- i
+}
+#clusters <- cluster(mat,clusters)
+while(length(unique(clusters)) > 1) {
+  mat <- list()
+  val <- unique(clusters)
+  for(i in 1:length(val)) {
+    clust <- list()
+    k = 1
+    for(j in 1:length(clusters)) {
+      if(clusters[[j]] == val[i]) {
+        clust[[k]] <- data[j,]
+        k = k + 1
+      }
+    }
+    mat[[i]] <- get_centroid(clust)
+  }
+  clusters <- cluster(mat,clusters)
+}
+
