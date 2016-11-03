9ef227f201e670de640d1e72329c9fd77a5e3490
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-11-03 23:31:17 +0530

    Adding initial Clustering metrics

--- hierarchical.r
+++ hierarchical.r
@@ -1,6 +1,28 @@
 setwd('~/Code/SMAI/KNN')
-cluster <- function(data) {
-  
+manhattan_distance <- function(p1,p2) {
+  return(sum(p1 - p2))
 }
 
-data <- as.matrix(read.csv("Datasets/movement_librars_1.data",header = FALSE, sep = " ",fill = FALSE))
+squared_euclidean_distance <- function(p1,p2) {
+  return(sum((p1 - p2)**2))
+}
+
+cluster <- function(data) {
+  min_val = manhattan_distance(data[0,],data[1,])
+  min_i = 0
+  min_j = 1
+  for(i in 1:nrow(data)) {
+    for(j in 1:nrow(data)) {
+      if( i != j) {
+          distance = manhattan_distance(data[i,],data[j,])
+          if(distance < min_val) {
+            min_val = distance
+            min_i = i
+            min_j = j
+          }
+      }
+    }
+  }
+}
+
+data <- as.matrix(read.csv("Datasets/movement_libras_1.data",header = FALSE, sep = ",",fill = FALSE))
