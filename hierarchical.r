9e63def75507bc51abfa8a3e94e3136450f62299
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-11-04 02:55:18 +0530

    WOrking bugfixed

--- hierarchical.r
+++ hierarchical.r
@@ -1,6 +1,6 @@
 setwd('~/Code/SMAI/KNN')
 manhattan_distance <- function(p1,p2) {
-  return(sum(p1 - p2))
+  return(abs(sum(p1 - p2)))
 }
 
 squared_euclidean_distance <- function(p1,p2) {
@@ -8,34 +8,47 @@ squared_euclidean_distance <- function(p1,p2) {
 }
 
 get_centroid <- function(l) {
-  return(Reduce("+",l)/length(l))
+  if(length(l) > 91) {
+    v <- apply(l,2,function(x) sum(x))
+    v <- v/nrow(l)
+  } else {
+    v <- l
+  }
+  return(v)
 }
 
 cluster <- function(data,clusters) {
-  min_val = manhattan_distance(data[[1]],data[[2]])
+  reduced_set <- unique(clusters)
+  min_val = squared_euclidean_distance(data[[1]],data[[2]])
   min_i = 1
   min_j = 2
+  min_val_i = reduced_set[[1]]
+  min_val_j = reduced_set[[2]]
   for(i in 1:length(data)) {
-    for(j in 1:length(data)) {
+    for(j in i:length(data)) {
       if( i != j) {
-          distance = manhattan_distance(data[[i]],data[[j]])
-          if(distance < min_val) {
+          distance = squared_euclidean_distance(data[[i]],data[[j]])
+          if(distance <= min_val) {
             min_val = distance
             min_i = i
             min_j = j
+            min_val_i = reduced_set[[i]]
+            min_val_j = reduced_set[[j]]
           }
       }
     }
   }
-  
-  clusters[[min_i]] <- min_i 
-  clusters[[min_j]] <- min_i 
+  for(i in 1:length(clusters)) {
+    if(clusters[[i]] == min_val_i || clusters[[i]] == min_val_j) {
+      clusters[[i]] <- min_i   
+    }
+  }
   return(clusters)
 }
 
 data <- as.matrix(read.csv("Datasets/movement_libras_1.data",header = FALSE, sep = ",",fill = FALSE))
-clusters <- list()
-for(i in 1:500) {
+clusters <- vector('integer')
+for(i in 1:45) {
   #mat[[i]] <- data[i,]
   clusters[[i]] <- i
 }
@@ -44,16 +57,9 @@ while(length(unique(clusters)) > 1) {
   mat <- list()
   val <- unique(clusters)
   for(i in 1:length(val)) {
-    clust <- list()
-    k = 1
-    for(j in 1:length(clusters)) {
-      if(clusters[[j]] == val[i]) {
-        clust[[k]] <- data[j,]
-        k = k + 1
-      }
-    }
-    mat[[i]] <- get_centroid(clust)
+    mat[[i]] <- get_centroid(data[clusters == val[i],])
   }
+  print(length(val))
   clusters <- cluster(mat,clusters)
 }
 
